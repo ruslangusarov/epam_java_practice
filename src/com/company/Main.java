a43a80fe78524c5b159bbd233fbe2f6bb566fe80
Ruslan Gusarov <ruslan.gusarov@internet.ru>
2022-01-14 18:45:21 +0300

    More clean code.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,6 @@ package com.company;
 
 import java.util.Scanner;
 import java.lang.*;
-import java.io.*;
 
 public class Main {
 
@@ -17,7 +16,7 @@ public class Main {
         for (int i=txet.length, a=0; i>0; i--, a++) {
             faketxet[a] = txet[i-1];
         }
-        System.out.println("Did you mean "+String.valueOf(faketxet)+"? I'm joking, I know you definetely meant that. Now give me a number:");
+        System.out.println("Did you mean "+String.valueOf(faketxet)+"? I'm joking, I know you definitely meant that. Now give me a number:");
         int number = scanner.nextInt();
         int [] numbers = new int[number];
         System.out.println("Okay, "+number+" new numbers are generated: ");
@@ -26,8 +25,8 @@ public class Main {
             System.out.println(numbers[i]);
         }
         System.out.println("I mean seriously, here they are from top to bottom: ");
-        for (int i=0; i<numbers.length; i++) {
-            System.out.print(numbers[i] + " ");
+        for (int j : numbers) {
+            System.out.print(j + " ");
         }
         System.out.println();
         System.out.println("Now let's do some calculations! Give me 2 natural numbers and I'll try to add and multiply them!");
@@ -42,26 +41,20 @@ public class Main {
         }
         System.out.println("Now let's speak about months. Which month is your favourite? Give me its number: ");
         int month = scanner.nextInt();
-        switch(month) {
-            case 1: System.out.println("Oh, January! The first of the year, the second of winter! Like this too!"); break;
-            case 2: System.out.println("February, wow! The coldest one I suppose, but it is already the end of winter!"); break;
-            case 3: System.out.println("March! Spring starts from it and nature revives all around!"); break;
-            case 4: System.out.println("Oh, April! But, why though? Because of April's Fools? Or your B-Day is near?"); break;
-            case 5: System.out.println("May! Not summer, but almost not spring! Or still spring?..."); break;
-            case 6: System.out.println("June! Gotcha! Summer, sun, beaches and... So, you don't study in the Uni, right?"); break;
-            case 7: System.out.println("July is the middle of the year and of summer! Definetely understand you, the warmest or even hottest one!"); break;
-            case 8: System.out.println("August! Seems very romantic to me... Don't know why, I'm just a machine..."); break;
-            case 9: System.out.println("September? Really? So, you're not a student any more, right?"); break;
-            case 10: System.out.println("October? Hm-m... Have you ever been to Germany? Do you like beer? Isn't it the reason?"); break;
-            case 11: System.out.println("November is this type of month when you can hit the jackpot during the sales, got it!"); break;
-            case 12: System.out.println("So, December, as the beginning of winter and the end of the year! Interesting!"); break;
-            default: System.out.println("But there are just 12 months in the year... In each year, I suppose... You kidding!"); break;
+        switch (month) {
+            case 1 -> System.out.println("Oh, January! The first of the year, the second of winter! Like this too!");
+            case 2 -> System.out.println("February, wow! The coldest one I suppose, but it is already the end of winter!");
+            case 3 -> System.out.println("March! Spring starts from it and nature revives all around!");
+            case 4 -> System.out.println("Oh, April! But, why though? Because of April's Fools? Or your B-Day is near?");
+            case 5 -> System.out.println("May! Not summer, but almost not spring! Or still spring?...");
+            case 6 -> System.out.println("June! Gotcha! Summer, sun, beaches and... So, you don't study in the Uni, right?");
+            case 7 -> System.out.println("July is the middle of the year and of summer! Definitely understand you, the warmest or even hottest one!");
+            case 8 -> System.out.println("August! Seems very romantic to me... Don't know why, I'm just a machine...");
+            case 9 -> System.out.println("September? Really? So, you're not a student any more, right?");
+            case 10 -> System.out.println("October? Hm-m... Have you ever been to Germany? Do you like beer? Isn't it the reason?");
+            case 11 -> System.out.println("November is this type of month when you can hit the jackpot during the sales, got it!");
+            case 12 -> System.out.println("So, December, as the beginning of winter and the end of the year! Interesting!");
+            default -> System.out.println("But there are just 12 months in the year... In each year, I suppose... You kidding!");
         }
-
-
-
-
-
-
     }
 }
