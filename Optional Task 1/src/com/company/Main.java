efd8fbc2a99c33b6c81b735e62d573376ed0ae2a
Ruslan Gusarov <ruslan.gusarov@internet.ru>
2022-01-16 01:15:50 +0300

    Tried to make Case 4

--- Optional Task 1/src/com/company/Main.java
+++ Optional Task 1/src/com/company/Main.java
@@ -31,6 +31,7 @@ public class Main {
             }
         }
             switch (exercise) {
+
                 case 1:
                     System.out.println("Самое длинное число (наибольшее): "+number[number.length-1]);
                     System.out.println("Самое короткое число (наименьшее): "+number[0]);
@@ -46,6 +47,7 @@ public class Main {
                         System.out.print(number[i] + " ");
                     }
                     break;
+
                 case 3:
                     int line = 1;
                     int midline = 0;
@@ -84,6 +86,52 @@ public class Main {
                         }
                     }
                     break;
+
+                /* case 4:
+                    int [] same = new int [number.length];
+                    int n0, n1, n2, n3, n4, n5, n6, n7, n8, n9;
+                    for (int i=0;i<number.length-1;i++) {
+                        int line = 1;
+                        int [] numberLine = new int [number.length];
+                        for (int c=0; c<number.length; c++) {
+                            numberLine[c]=9;
+                            while (number[c]>numberLine[c]) {
+                                line++;
+                                numberLine[c]=(numberLine[c]+1)*10-1;
+                                continue;
+                            }
+                            numberLine[c]=line;
+                            line=1;
+                        }
+                        for (int c=0;c<number.length;c++) {
+                            switch (numberLine[c]) {
+                                case 1:
+                                    same[c]=numberLine[c];
+                                    break;
+                                case 2:
+                                    n1 = number[c]/10;
+                                    n0 = number[c]-n1*10;
+                                    same[c]=n0==n1?1:2;
+                                    break;
+                                case 3:
+                                    n2 = number[c]/100;
+                                    n1 = (number[c]-n2*100)/10;
+                                    n0 = number[c]-(n2*100+n1*10);
+                                    same[c]=n2==n1==n0?1:0;
+                                    same[c]=same[c]==0&&(n0==n1||n1==n2||n2==n0)?2:3;
+                                    break;
+                                case 4:
+                                    n3 = number[c]/1000;
+                                    n2 = (number[c]-n3*1000)/100;
+                                    n1 = (number[c]-(n3*1000+n2*100))/10;
+                                    n0 = number[c]-(n3*1000+n2*100+n1*10);
+                                    same[c]=n3==n2==n1==n0?1:0;
+                                    same[c]=same[c]==0&&(n0==n1||n1==n2||n2==) */
+
+                            }
+                        }
+                    }
+
             }
     }
 }
